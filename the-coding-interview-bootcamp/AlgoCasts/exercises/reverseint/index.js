660fa5d07a0e89e3930d7076dc74314e28f8e6a7
korrollir <mcmcconn@gmail.com>
2018-04-13 22:55:07 -0400

    algos - reverseint solved

--- the-coding-interview-bootcamp/AlgoCasts/exercises/reverseint/index.js
+++ the-coding-interview-bootcamp/AlgoCasts/exercises/reverseint/index.js
@@ -8,6 +8,28 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  /* My initial solution
+
+    Can do the longer version of splitting then looping through to reverse.
+    let reversed = n.toString().split('').reverse();
+
+    for (character of reversed) {
+      if (character === '0') {
+        reversed.shift();
+      }
+    }
+
+    if (Math.sign(n) === -1) {
+      reversed.unshift('-');
+    } 
+    
+    return parseInt(reversed.join(''), 10);
+  */
+  /* Grider's solution */
+  const reversed =  n.toString().split('').reverse().join('');
+
+  return parseInt(reversed) * Math.sign(n);
+}
 
 module.exports = reverseInt;
