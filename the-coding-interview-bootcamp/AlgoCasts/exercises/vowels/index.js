191bafe322640b9c2f0c81ba557bfda1029b60bf
korrollir <mcmcconn@gmail.com>
2018-04-17 22:58:57 -0400

    Completed vowels algo

--- the-coding-interview-bootcamp/AlgoCasts/exercises/vowels/index.js
+++ the-coding-interview-bootcamp/AlgoCasts/exercises/vowels/index.js
@@ -7,6 +7,25 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  /* My Iterative Solution */
+  // const comparison = ['a', 'e', 'i', 'o', 'u'];
+  // let count = 0;
+
+  // for (let char of str.toLowerCase()) {
+  //   if (comparison.includes(char)) {
+  //     count++;
+  //   }
+  // }
+
+  // return count;
+
+  /* RegEx Solution */
+  // g ensures we do not stop at first match in the string
+  // i stands for case insensitive
+  const matches = str.match(/[aeiou]/gi);
+
+  return matches ? matches.length : 0;
+}
 
 module.exports = vowels;
