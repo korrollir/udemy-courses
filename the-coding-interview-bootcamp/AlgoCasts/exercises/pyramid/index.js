76a7f1738439be06e675d53196b5a933a3486554
korrollir <mcmcconn@gmail.com>
2018-04-17 22:01:20 -0400

    Complete pyramid algo with recursion

--- the-coding-interview-bootcamp/AlgoCasts/exercises/pyramid/index.js
+++ the-coding-interview-bootcamp/AlgoCasts/exercises/pyramid/index.js
@@ -14,6 +14,48 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, tier = '') {
+  /* Iterative Solution */
+  // // Calculate midpoint of the pyramid
+  // const midpoint = Math.floor((2 * n - 1) / 2);
+  // // Start a row
+  // for (let row = 0; row < n; row++) {
+  //   // Create a fresh stair string
+  //   let tier = '';
+  //   // Iterate through columns to assign the correct character
+  //   // Each step has a width of n + n - 1 (e.g. 3 steps is 5 wide, 4 is 7, etc)
+  //   for (let column = 0; column < 2 * n - 1; column++) {
+  //     // Determine whether to add a # or a space
+  //     // The midpoint boolean sets bounds
+  //     if (midpoint - row <= column && midpoint + row >= column) {
+  //       tier += '#';
+  //     } else {
+  //       tier += ' ';
+  //     }
+  //   }
+
+  //   console.log(tier);
+  // }
+
+  /* Recursive Solution */
+  const midpoint = Math.floor((2 * n - 1) / 2);
+  // Base case
+  if (row === n) {
+    return;
+  }
+  // Case for hitting end of the row
+  if (2 * n - 1 === tier.length) {
+    console.log(tier);
+    return pyramid(n, row + 1);
+  }
+  // Modify the tier based on position
+  if (midpoint - row <= tier.length && midpoint + row >= tier.length) {
+    tier += '#';
+  } else {
+    tier += ' ';
+  }
+  
+  pyramid(n, row, tier);
+}
 
 module.exports = pyramid;
