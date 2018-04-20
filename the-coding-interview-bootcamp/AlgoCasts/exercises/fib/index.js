485e45b51373c4f4cc68e9c33aef9bb69ea2dbdd
korrollir <mcmcconn@gmail.com>
2018-04-19 23:25:16 -0400

    Complete iterative, recursive, and memoized fibonacci sequence

--- the-coding-interview-bootcamp/AlgoCasts/exercises/fib/index.js
+++ the-coding-interview-bootcamp/AlgoCasts/exercises/fib/index.js
@@ -8,6 +8,55 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+
+/* TODO: Memorize Memoization solution */
+// Memoization - store the arguments of each function call along with the result.
+// If the function is called again with the same arguments, return the precomputed
+// result, rather than running the function again.
+// Slow fib function -> Memoizer function -> Fast, memoized fib function
+function memoize(fn) {
+  const cache = {};
+
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+    // Call slowFib with the supplied argument of the original call
+    const result = fn.apply(this, args);
+    // Store the result in the cache object
+    cache[args] = result;
+
+    return result;
+  }
+}
+
+function slowFib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memoize(slowFib);
+
+// function fib(n) {
+//   /* Iterative solution - O(n) */
+//   // const fibSequence = [0, 1];
+
+//   // for (let i = 1; i <= n; i++) {
+//   //   fibSequence.push(fibSequence[i] + fibSequence[i - 1]);
+//   // }
+
+//   // return fibSequence[n];
+  
+//   /* TODO: Memorize Recursive solution - O(2 ^ n) */
+//   // Base case
+//   if (n < 2) {
+//     return n;
+//   }
+
+//   return fib(n - 1) + fib(n - 2);
+// }
 
 module.exports = fib;
