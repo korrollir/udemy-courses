4acd0eef1d71a573cf0d5999228eee25799560db
korrollir <mcmcconn@gmail.com>
2018-04-20 19:24:37 -0400

    rename function for readability

--- the-coding-interview-bootcamp/AlgoCasts/exercises/fib/index.js
+++ the-coding-interview-bootcamp/AlgoCasts/exercises/fib/index.js
@@ -30,14 +30,13 @@ function memoize(fn) {
   }
 }
 
-function slowFib(n) {
+function fib(n) {
   if (n < 2) {
     return n;
   }
 
   return fib(n - 1) + fib(n - 2);
-}
-
+  
 const fib = memoize(slowFib);
 
 // function fib(n) {
