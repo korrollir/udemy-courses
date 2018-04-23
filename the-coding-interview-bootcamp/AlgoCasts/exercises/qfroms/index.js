74a150d7d34249b05471a61d455a44deb547501f
korrollir <mcmcconn@gmail.com>
2018-04-23 00:03:58 -0400

    Completed queue from stack

--- the-coding-interview-bootcamp/AlgoCasts/exercises/qfroms/index.js
+++ the-coding-interview-bootcamp/AlgoCasts/exercises/qfroms/index.js
@@ -14,6 +14,45 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  /* Remember this is about stacking and unstacking data entries within the stacks 
+    to simulate use a queue */
+  constructor() {
+    this.dataStack = new Stack();
+    this.tempStack = new Stack();
+  }
+
+  add(record) {
+    this.dataStack.push(record);
+  }
+
+  remove() {
+    while (this.dataStack.peek()) {
+      this.tempStack.push(this.dataStack.pop());
+    }
+    // Be careful to set this variable AFTER adding to tempStack
+    const record = this.tempStack.pop();
+
+    while (this.tempStack.peek()) {
+      this.dataStack.push(this.tempStack.pop());
+    }    
+
+    return record;
+  }
+
+  peek() {    
+    while (this.dataStack.peek()) {
+      this.tempStack.push(this.dataStack.pop());
+    }
+    // Be careful to set this variable AFTER addinng to tempStack
+    const record = this.tempStack.peek();
+
+    while (this.tempStack.peek()) {
+      this.dataStack.push(this.tempStack.pop());
+    }
+
+    return record;
+  }
+}
 
 module.exports = Queue;
