32b0f98ab25f9dfeb8b0ce6a9abb72a86e253728
korrollir <mcmcconn@gmail.com>
2018-05-01 21:59:03 -0400

    Completed linked list midpoint

--- the-coding-interview-bootcamp/AlgoCasts/exercises/midpoint/index.js
+++ the-coding-interview-bootcamp/AlgoCasts/exercises/midpoint/index.js
@@ -12,6 +12,21 @@
 //   l.insertLast('c')
 //   midpoint(l); // returns { data: 'b' }
 
-function midpoint(list) {}
+function midpoint(list) {
+  // Create two temp vars (slow and fast) that point at the first node of the list.
+  let slow = list.head;
+  let fast = list.head;
+
+  // When the fast pointer receives null on fast.next or fast.next.next, slow will be 
+  // at the midpoint
+  while(fast.next && fast.next.next) {
+    // For each step of the loop, the slow pointer will move forward one place, and
+    // the fast pointer will move forward two.
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+}
 
 module.exports = midpoint;
