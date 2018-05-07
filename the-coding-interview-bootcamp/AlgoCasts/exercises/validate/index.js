efb4555fd746d5a96a131995f8a3d42ec6c7c2b2
korrollir <mcmcconn@gmail.com>
2018-05-06 22:50:01 -0400

    Complete BST validate

--- the-coding-interview-bootcamp/AlgoCasts/exercises/validate/index.js
+++ the-coding-interview-bootcamp/AlgoCasts/exercises/validate/index.js
@@ -5,6 +5,26 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  // Recursive Solution
+  // Base case
+  if (max !== null && node.data > max) {
+    return false;
+  }
+
+  if (min !== null && node.data < min) {
+    return false;
+  }
+  // Compare to left and verify the node.left < node.data
+  if (node.left && !validate(node.left, min, node.data)) {
+    return false;
+  }
+  // Compare to right and verify the node.right > node.data
+  if (node.right && !validate(node.right, node.data, max)) {
+    return false;
+  }
+
+  return true;
+}
 
 module.exports = validate;
