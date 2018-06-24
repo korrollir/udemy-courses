7d902faae4ac3a37790b94e1f45d3e074340486e
korrollir <mcmcconn@gmail.com>
2018-06-24 02:23:26 -0400

    Complete through 41

--- Advanced-React-Redux/testing-jest-enzyme/src/components/__tests__/CommentBox.test.js
+++ Advanced-React-Redux/testing-jest-enzyme/src/components/__tests__/CommentBox.test.js
@@ -20,13 +20,30 @@ it('has a textarea and a button', () => {
   expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a textarea that users can type in', () => {
-  wrapped.find('textarea').simulate('change', {
-    target: { value: 'new comment'} // Emulates event.target.value
+// describe groups together tests and limits the scope of beforeEach/afterEach statements
+describe('the textarea', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' } // Emulates event.target.value
+    });
+    // Force the component to update state
+    wrapped.update();
+    // Assert with the correct 'value' prop
   });
-  // Force the component to update state
-  wrapped.update();
-  // Assert with the correct 'value' prop
-  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-});
+
+  it('allows users to type in it', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
   
+  it('empties when form is submitted', () => {
+    // This would be good form to test the textarea changed, but it is covered
+    // in the above test.
+    // expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+    // Submit the form
+    wrapped.find('form').simulate('submit');
+    // Update following submission to update state
+    wrapped.update();
+    // Assert the textarea is empty after form submission
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
+});
