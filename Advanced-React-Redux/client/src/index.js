83b2a3648d5d07daf2876358e0be8d5802f2d2ea
korrollir <mcmcconn@gmail.com>
2018-07-17 01:01:34 -0400

    Redux and redux-form boilerplate

--- Advanced-React-Redux/client/src/index.js
+++ Advanced-React-Redux/client/src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
+import reducers from './reducers';
 import App from './components/App';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App>
-      <Route exact path='/' component={Welcome} />
-      <Route path='/signup' component={Signup} />
-    </App>
-  </BrowserRouter>,
+  <Provider store={createStore(reducers, {})}>
+    <BrowserRouter>
+      <App>
+        <Route exact path='/' component={Welcome} />
+        <Route path='/signup' component={Signup} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
   document.querySelector('#root')
 );
 
